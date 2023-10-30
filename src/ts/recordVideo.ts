37865f5c7b6fa5547f42eb4c0d2915085047c5a4
Manu <36305839+ManuelRodriguez1@users.noreply.github.com>
2023-10-30 16:49:06 -0500

    Short URL
    Probando con back para acortar URL

--- src/ts/recordVideo.ts
+++ src/ts/recordVideo.ts
@@ -1,4 +1,5 @@
 import Swal from "sweetalert2";
+// import { POST } from '../pages/api/shortUrl.json.ts'
 
 const audioPublicId = 'n8a4qzsxs1anb4j3nfbw'
 const imgPublicId = 'dz6cbwuny3pvgquexxuz'
@@ -6,11 +7,11 @@ const videoPublicId = 'j6cgvsuxd6jzaou5dy4n'
 const _urlImage = `upload/c_pad,h_1350,w_1080/l_img:${ imgPublicId }/fl_layer_apply/l_video:videoAlpha:${  videoPublicId }/co_green,e_make_transparent:14/fl_layer_apply/l_audio:audio:${ audioPublicId }/fl_layer_apply`
 const url: string = "https://api.cloudinary.com/v1_1/didbn5pv6/auto/upload";  
 const formData = new FormData();
-const timer: number = 10; //cantidad de seg
+const timer: number = 62 //cantidad de seg 62
 
 //https://res.cloudinary.com/didbn5pv6/video/upload/c_pad,h_1350,w_1080/l_img:dz6cbwuny3pvgquexxuz/fl_layer_apply/l_video:videoAlpha:j6cgvsuxd6jzaou5dy4n/co_green,e_make_transparent:14/fl_layer_apply/l_audio:audio:svipo909nrnmilfpeqiw/fl_layer_apply/v1698266224/gphk9ynqlw7uaflee6io.mp4
 
-
+const screenLoading: any = document.querySelector('#screenLoading');
 const videoRef: any = document.querySelector('#videoRecording');
 const audioSource: string = '';
 const videoSource: string = '';
@@ -111,6 +112,8 @@ const uploadFile = ( blob: Blob ) => {
 
     formData.append("file", blobToFile( blob ));
     formData.append("upload_preset", "zt69ebf3");
+    screenLoading.classList.remove('opacity-0')
+
 
     fetch(url, {
         method: "POST",
@@ -123,7 +126,8 @@ const uploadFile = ( blob: Blob ) => {
         const newUrl = secure_url.split('upload')
         _newUrl = newUrl[0]+_urlImage+newUrl[1]
 
-        shortUrl( _newUrl )
+        // shortUrl( _newUrl ) //TODO: Seguir probando
+        alertMessage( _newUrl ) //? Para que Jaime lo pruebe
     });
 
 }
@@ -135,27 +139,24 @@ const blobToFile = ( blob: Blob, filename: string = 'newVideo.mov') => {
     return blob as File;
 }
 
-const shortUrl = ( _url: string ) => {
-    const _newUrl = `https://ulvis.net/api.php?url=${ _url }&private=1&type=json`
-    // const _newUrl = `https://ulvis.net/api.php?url=`
-    console.log(_newUrl)
-    fetch( _newUrl, {
-        // method: 'POST',
-        // headers: new Headers({
-        //     'Content-Type': 'application/json',
-        //     'Access-Control-Allow-Origin': '*',
-        // }),
-        // body: _url
-    }).then( (  data: any ) => {
-        console.log( data )
-            // const { url } = data
-            // alertMessage( url );
-        })
+const shortUrl = async ( _url: any ) => {
 
+    
+    fetch(window.location+'api/shortUrl.json', {
+        body: JSON.stringify({
+            "domain": "l.rblab.io",
+            "originalURL": _url
+        })
+    }).then( ( response ) => {
+        console.log(response)
+    })
+    //    let resp = await POST( _url )
+    //    console.log( resp )
 }
 
 
 const alertMessage = ( url: string ) => {
+    screenLoading.classList.add('opacity-0')
     Swal.fire({
         title: 'Video Created',
         html: `Send your video by Email<br /><br /> <a href="mailto:youremail@here.com?subject=Video%20Redbull&body=Download%20video%20here:%20${ url }" style='text-decoration: underline;'>Click here</a>`,
