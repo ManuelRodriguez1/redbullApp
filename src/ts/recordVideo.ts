c5bcafbda513e823efe37065dfeeeb1ed0f3047f
Manu <36305839+ManuelRodriguez1@users.noreply.github.com>
2023-11-17 15:00:04 -0500

    Update
    Se elimina un alert y se genera un codigo QR de la URL

--- src/ts/recordVideo.ts
+++ src/ts/recordVideo.ts
@@ -1,4 +1,5 @@
-import { alertMessage } from "./alertMessage";
+import Swal from "sweetalert2";
+
 const _urlImage = `upload/t_rb`
 const url: string = "https://api.cloudinary.com/v1_1/didbn5pv6/auto/upload";  
 const formData = new FormData();
@@ -18,6 +19,8 @@ let audio: any = document.getElementById("beat");
 let _newUrl: string = '';
 let counter: number = 1;
 let _isActive: boolean = false;
+
+
 
 ( async () => {
         
@@ -157,14 +160,15 @@ const uploadFile = ( blob: Blob ) => {
         body: formData
     }).then(( response ) => {
         return response.text();
-    }).then(( data ) => {
+    }).then(async ( data ) => {
         
         const { secure_url } = JSON.parse( data )
         const newUrl = secure_url.split('upload')
         _newUrl = newUrl[0]+_urlImage+newUrl[1]
-
+        
         alertMessage( _newUrl ) 
         btnStop.click();
+        
     });
 
 }
@@ -174,4 +178,33 @@ const blobToFile = ( blob: Blob, filename: string = 'newVideo.mov') => {
     b.lastModifiedDate = new Date();
     b.name = filename;
     return blob as File;
+}
+
+
+const alertMessage = async ( url: string ) => {
+    screenLoading.classList.add('opacity-0')
+    
+    setTimeout(() => {
+        const qrImage: any = document.querySelector('.qrCode');
+        new QRCode(qrImage, {
+            text: url,
+          });
+    }, 1000);
+
+    Swal.fire({
+        allowOutsideClick: false,
+        title: `<div class="w-28 h-28 border-[6px] border-[#0A2E89] rounded-full mx-auto flex justify-center items-center"><img src="../images/checkIcon.svg" /></div>
+        <h2 class=" -mt-3 font-bold font-Adieu uppercase text-[26px] text-[#0A2E89]">Video finalizado</h2>`,
+        html: `<p class="text-sm font-Adieu uppercase w-[363px] mx-auto text-[#0A2E89]">Presiona el botón de “enviar” para recibir el link de descarga en tu correo electrónico.</p>
+        <div class="flex justify-center mt-8"><div class="qrCode w-32 h-32" ></div></div>`,
+        reverseButtons: true,
+        showDenyButton: true,
+        denyButtonText: `<svg width="18" height="18" viewBox="0 0 18 18" fill="none">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M2.72377 7.99118C2.55247 8.1462 2.4547 8.36642 2.45459 8.59745V14.3133C2.45549 14.5415 2.64032 14.7264 2.86859 14.7273H6.95459C7.18052 14.7273 7.36368 14.5441 7.36368 14.3182L7.36859 10.2273C7.36859 10.0013 7.55175 9.81818 7.77768 9.81818H10.2224C10.3323 9.81796 10.4377 9.86151 10.5154 9.9392C10.5931 10.0169 10.6366 10.1223 10.6364 10.2322V14.3182C10.6364 14.5441 10.8196 14.7273 11.0455 14.7273H15.1364C15.3623 14.7273 15.5455 14.5441 15.5455 14.3182V8.59745C15.5454 8.36642 15.4476 8.1462 15.2763 7.99118L9.2725 2.56009C9.11729 2.42152 8.8828 2.42152 8.72759 2.56009L2.72377 7.99118Z" fill="#000F1E"/>
+         </svg>`,
+        denyButtonColor: '#000f1e0d',
+        confirmButtonText: `<span class="flex gap-2 items-center">Finalizar</span>`,
+        confirmButtonColor: "#DB0A40",
+        footer: `<p class="font-bull text-[#0A2E89] w-[275px] mx-auto text-xs">Espera de 10 a 20 minutos para recibir el link de descarga del video en tu correo electrónico.</p>`
+      })
 }
